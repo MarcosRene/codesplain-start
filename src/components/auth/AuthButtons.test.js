57d61dbdc2bf51d4aa8efc78ee5d4753f7fe8260
MarcosRene <renemoura789@gmail.com>
2023-09-27 15:02:43 -0300

    fix AuthButtons test file

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
+import { SWRConfig } from "swr";
 
 import { createServer } from "../../test/server";
 
@@ -7,15 +8,59 @@ import AuthButtons from "./AuthButtons";
 
 const renderAuthButtons = async () => {
   render(
-    <MemoryRouter>
-      <AuthButtons />
-    </MemoryRouter>
+    <SWRConfig value={{ provider: () => new Map() }}>
+      <MemoryRouter>
+        <AuthButtons />
+      </MemoryRouter>
+    </SWRConfig>
   );
 
   await screen.findAllByRole("link");
 };
 
 describe("<AuthButtons />", () => {
+  describe("when user is signed in", () => {
+    createServer([
+      {
+        path: "/api/user",
+        res: () => {
+          return {
+            user: {
+              id: parseInt(Math.random() * 10),
+              email: "johndoe@johndoe.com",
+            },
+          };
+        },
+      },
+    ]);
+
+    it("should not render the buttons Sign in and Sign up", async () => {
+      await renderAuthButtons();
+
+      const signInButton = screen.queryByRole("link", {
+        name: /sign in/i,
+      });
+
+      const signUpButton = screen.queryByRole("link", {
+        name: /sign up/i,
+      });
+
+      expect(signInButton).not.toBeInTheDocument();
+      expect(signUpButton).not.toBeInTheDocument();
+    });
+
+    it("should render the button Sign out", async () => {
+      await renderAuthButtons();
+
+      const signOutButton = screen.getByRole("link", {
+        name: /sign out/i,
+      });
+
+      expect(signOutButton).toBeInTheDocument();
+      expect(signOutButton).toHaveAttribute("href", "/signout");
+    });
+  });
+
   describe("when user is not signed in", () => {
     createServer([
       {
@@ -52,48 +97,6 @@ describe("<AuthButtons />", () => {
       });
 
       expect(signOutButton).not.toBeInTheDocument();
-    });
-  });
-
-  describe("when user is signed in", () => {
-    createServer([
-      {
-        path: "/api/user",
-        res: () => {
-          return {
-            user: {
-              id: parseInt(Math.random() * 10),
-              email: "johndoe@johndoe.com",
-            },
-          };
-        },
-      },
-    ]);
-
-    it("should not render the buttons Sign and Sign up", async () => {
-      await renderAuthButtons();
-
-      const signInButton = screen.queryByRole("link", {
-        name: /sign in/i,
-      });
-
-      const signUpButton = screen.queryByRole("link", {
-        name: /sign up/i,
-      });
-
-      expect(signInButton).not.toBeInTheDocument();
-      expect(signUpButton).not.toBeInTheDocument();
-    });
-
-    it("should render the button Sign out", async () => {
-      await renderAuthButtons();
-
-      const signOutButton = screen.getByRole("link", {
-        name: /sign out/i,
-      });
-
-      expect(signOutButton).toBeInTheDocument();
-      expect(signOutButton).toHaveAttribute("href", "/signout");
     });
   });
 });
